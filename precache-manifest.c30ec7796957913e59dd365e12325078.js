fcc9d42909c32b9904ab7b1b7e35778a126ad0b2
ofk8vb <latios_gs@hotmail.com>
2020-08-13 14:12:09 +0300

    Updates

---
+++ precache-manifest.c30ec7796957913e59dd365e12325078.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5600b846e5f1392576bc2c57eb32fbf8",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bd2a90529b51321f4568",
+    "url": "/monster-rolodex/static/css/main.75f3a1ea.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/monster-rolodex/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd2a90529b51321f4568",
+    "url": "/monster-rolodex/static/js/main.736ee1ef.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);

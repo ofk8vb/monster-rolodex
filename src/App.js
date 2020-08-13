3afd2b82e0c8c85287cfa25151971fbb1c755ee8
ofk8vb <latios_gs@hotmail.com>
2020-08-13 14:00:37 +0300

    finished project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import {CardList} from './components/card-list/card-list.component.jsx'
+import {SearchBox} from './components/search-box/search-box.component'
 import './App.css';
 
 
@@ -9,8 +10,13 @@ class App extends Component {
 
 
     this.state={
-        monsters: []
+        monsters: [],
+        searchField:''
     };
+
+    this.handleChange=this.handleChange.bind(this)
+
+
   }
 
   componentDidMount(){
@@ -19,11 +25,27 @@ class App extends Component {
       .then(users => this.setState({monsters: users}));
   }
 
+  handleChange=(e)=>{
+    this.setState({searchField:e.target.value})
+
+  }
+
 
   render(){
+    const{ monsters,searchField}=this.state;
+    const filteredMonster = monsters.filter(monster=>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+        <h1>Monsters Rolodex</h1>
+       <SearchBox
+        placeholder='search monsters'
+        handleChange={this.handleChange}
+       />
+        <CardList monsters={filteredMonster} />
       </div>
     );
   }
